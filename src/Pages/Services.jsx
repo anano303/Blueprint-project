24934cca4b9fd424db5e5ec8f4d582504acd2721
anano303 <124179773+anano303@users.noreply.github.com>
2024-02-21 15:07:55 +0400

    carousel

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,18 +1,91 @@
-import "./servicies.css";
+import React, { useRef } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import image1 from "../Assets/პოლიგრაფია.jpg";
+import image3 from "../Assets/სასაჩუქრე.jpg";
+import image4 from "../Assets/სოციალური ქსელები.jpg";
+import image5 from "../Assets/პროდაქშენი.jpg";
+import image6 from "../Assets/პოლიგრაფია.jpg";
+import "./servicies.css"; // Import your CSS file
 
 const Services = () => {
+  const images = [image1, image3, image4, image5, image6];
+  const carouselRef = useRef(null);
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+      slidesToSlide: 5,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+      slidesToSlide: 4,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 3,
+      slidesToSlide: 3,
+    },
+  };
+
+  const handleNextClick = () => {
+    if (carouselRef.current) {
+      carouselRef.current.next();
+    }
+  };
+
   return (
     <div className="services">
-      აქ გვჭირდება კარუსელი, ლი-ს სურათები
       <h1>სერვისები</h1>
-      <ul>
-        <li>ბეჭდვა</li>
-        <li>პოლიგრაფია</li>
-        <li>სასაჩუქრე იდეები</li>
-        <li>სოციალური ქსელებისგადობარება/მართვა</li>
-        <li>ფორდაქშენი</li>
-        <li>პორთფოლიო</li>
-      </ul>
+      <div className="carousel-container">
+        <Carousel
+          ref={carouselRef}
+          responsive={responsive}
+          swipeable={true}
+          draggable={true}
+          showDots={true}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={3000}
+          keyBoardControl={true}
+          customTransition="transform 300ms ease-in-out"
+          transitionDuration={300}
+          containerClass="carousel"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          deviceType="desktop"
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
+          {images.map((image, index) => (
+            <div className="carousel-item" key={index}>
+              <li className="carousel-list-item">
+                <span className="text">
+                  {
+                    [
+                      "ბეჭდვა",
+                      "პოლიგრაფია",
+                      "სასაჩუქრე იდეები",
+                      "სოციალური ქსელებისგადობარება/მართვა",
+                      "პროდაქშენი",
+                      "პორტფოლიო",
+                    ][index]
+                  }
+                </span>
+                <img
+                  className="carousel-img"
+                  src={image}
+                  alt={`Service ${index + 1}`}
+                />
+              </li>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+      {/* <button className="arrow-button" onClick={handleNextClick}>
+        Next
+      </button> */}
     </div>
   );
 };
